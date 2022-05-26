e244a610ceb7d169c30c69b3e6ccdc7c5b0129ee
nick123245 <78725106+nick123245@users.noreply.github.com>
2022-05-26 15:14:54 +0500

    Update MainActivity.java
    Добавление RequiresApi. @RequiresApi(api = Build.VERSION_CODES.P) обозначает, что аннотированный элемент должен вызываться только на заданном уровне API или выше. 
    + Добавление библиотек

--- app/src/main/java/com/example/a2/MainActivity.java
+++ app/src/main/java/com/example/a2/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.sensors;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.ImageDecoder;
 import android.graphics.drawable.AnimatedImageDrawable;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -11,7 +13,7 @@ import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    @RequiresApi(api = Build.VERSION_CODES.P)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         ImageView imageView = findViewById(R.id.splash);
         loadGif(imageView);
     }
-    
+    @RequiresApi(api = Build.VERSION_CODES.P)
     private void loadGif(ImageView iv)  {
         try {        
             ImageDecoder.Source source;
