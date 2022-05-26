238875758b538188d0af9148493aed5221a00327
nick123245 <adept6573@gmail.com>
2022-05-26 14:20:59 +0500

    Add files

---
+++ app/src/main/java/com/example/a2/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.sensors;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.graphics.ImageDecoder;
+import android.graphics.drawable.AnimatedImageDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toast;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView imageView = findViewById(R.id.splash);
+        
+            ImageDecoder.Source source;
+            source = ImageDecoder.createSource(getResources(), R.drawable.ic_splash);
+
+            Drawable drawable;
+            drawable = ImageDecoder.decodeDrawable(source);
+            imageView.setImageDrawable(drawable);
+
+                ((AnimatedImageDrawable) drawable).start();
+                Toast.makeText(getApplicationContext(),
+                        "Animation started",
+                        Toast.LENGTH_LONG).show();
+
+        
+    }
+}
