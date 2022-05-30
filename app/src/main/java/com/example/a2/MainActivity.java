d543799d8030c03ec72f2cf4749827686e861655
nick123245 <78725106+nick123245@users.noreply.github.com>
2022-05-30 21:58:33 +0500

    Update MainActivity.java
    Обработка кнопок

--- app/src/main/java/com/example/a2/MainActivity.java
+++ app/src/main/java/com/example/a2/MainActivity.java
@@ -1,29 +1,52 @@
 package com.example.sensors;
 
 import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
 import android.graphics.ImageDecoder;
 import android.graphics.drawable.AnimatedImageDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 import java.io.IOException;
+import android.widget.Button;
+import android.view.View.OnClickListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
-    @RequiresApi(api = Build.VERSION_CODES.P)
+    Button button1;
+    Button button2;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
         ImageView imageView = findViewById(R.id.splash);
-        loadGif(imageView);
+        
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.P)
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.button1:
+                        loadGif1(imageView);
+                        break;
+                    case R.id.button2:
+                        loadGif2(imageView);
+                        break;
+                }
+            }
+
+        };
+        button1.setOnClickListener(onClickListener);
+        button2.setOnClickListener(onClickListener);
     }
     @RequiresApi(api = Build.VERSION_CODES.P)
-    private void loadGif(ImageView iv)  {
+    private void loadGif1(ImageView iv)  {
         try {        
             ImageDecoder.Source source = ImageDecoder.createSource(getResources(), R.drawable.ic_splash);
 
@@ -31,8 +54,8 @@ public class MainActivity extends AppCompatActivity {
             iv.setImageDrawable(drawable);
                 ((AnimatedImageDrawable) drawable).start();
                 Toast.makeText(getApplicationContext(),
-                        R.string.text1,
-                        Toast.LENGTH_LONG).show();
+                    "Animation Cynosure started",
+                    Toast.LENGTH_LONG).show();
         
             }
                 catch (IOException exception) {
@@ -41,6 +64,23 @@ public class MainActivity extends AppCompatActivity {
                         "IOException: \n" + exception.getMessage(),
                         Toast.LENGTH_LONG).show();
             }
-        
+    }
+        @RequiresApi(api = Build.VERSION_CODES.P)
+        private void loadGif2(ImageView iv) {
+        try {
+            ImageDecoder.Source source =
+                    ImageDecoder.createSource(getResources(), R.drawable.ic_splash2);
+            Drawable drawable = ImageDecoder.decodeDrawable(source);
+            iv.setImageDrawable(drawable);
+            ((AnimatedImageDrawable) drawable).start();
+            Toast.makeText(getApplicationContext(),
+                    "Animation Abstract started",
+                    Toast.LENGTH_LONG).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(),
+                    "IOException: \n" + e.getMessage(),
+                    Toast.LENGTH_LONG).show();
+        }
     }
 }
